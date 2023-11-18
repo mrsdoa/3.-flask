705920ac9d305cafaa6b033e3400fa7952080934
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-11-19 00:07:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ import flask
 from flask import jsonify
 from flask.views import MethodView
 from flask import request, Response
-#для того чтобы прочитать данные от клиента импортируем объект запросы
+from pydantic import ValidationError
+from schema import CreateAdvertisement
+
 from models import Advertisement, Session
 import psycopg2
 #объект класса
@@ -23,10 +25,16 @@ def error_handler(error):
     response.status_code = error.status_code
     return response
 
-#у app у экземпляра класса flask есть декоратор ниже
+def validate(model, data):
+    try:
+        return model.model_validate(data).model_dump(exclude_unset=True)
+    except ValidationError as err:
+        error = err.errors()[0]
+        error.pop('ctx', None)
+        raise HttpError(status_code=400, error)
+
 @app.before_request
 def before_request():
-    #создаём экземпляр класса сессии и добавляться к request
     session = Session()
     request.session = session
 
@@ -41,9 +49,6 @@ def get_advertisement(advertisement_id:int, session):
     advertisement = session.get(Advertisement, advertisement_id)
     if advertisement is None:
         raise HttpError(status_code=404, description='user not found')
-        # response = jsonify({'error': 'advertisement not found'})
-        # response.status_code = 404
-        # return response
     return advertisement
 
 def add_advertisement(advertisement: Advertisement):
@@ -61,44 +66,27 @@ class AdvertisementView(MethodView):
         return request.session
 
     def get(self, advertisement_id: int):
-        # advertisement = self.session.get(Advertisement, advertisement_id)
         advertisement = get_advertisement(advertisement_id)
-        # if advertisement_id is None:
-        #     response = jsonify({'error':'advertisement not found'})
-        #     response.status_code = 404
-        #     return response
         return jsonify({'id': advertisement.id, 'titile': advertisement.title, 'description': advertisement.description, 'owner': advertisement.owner, 'registration_time': advertisement.created_time.isoformat()})
 
     def post(self):
-        #получаем json от пользователя
-        user_request = request.json
-        # session = request.session
-        #экземпляр класса сессии
+        user_request = validate(CreateAdvertisement, request.json)
+        if 'title' not in user_request and 'description' not in user_request:
+            raise HttpError(status_code=400, description='title and description is required')
         new_advertisement = Advertisement(**user_request)
         new_advertisement = add_advertisement(new_advertisement)
-        # self.session.add(new_advertisement)
-        # self.session.commit()
         return jsonify({'id': new_advertisement.id})
+
     def patch(self, advertisement_id: int):
         user_request = request.json
         advertisement = get_advertisement(advertisement_id)
-        # session = request.session
-        #получаем юзера по id
-        # advertisement = self.session.get(Advertisement, advertisement_id)
-        # if advertisement is None:
-        #     response = jsonify({'error':'advertisement not found'})
-        #     response.status_code = 404
-        #     return response
         for key, value in user_request.items():
             setattr(advertisement, key, value)
         advertisement = add_advertisement(advertisement)
         # self.session.commit()
         return jsonify({'id': advertisement.id, 'titile': advertisement.title, 'description': advertisement.description, 'owner': advertisement.owner, 'registration_time': advertisement.created_time.isoformat()})
     def delete(self, advertisement_id: int):
-        # session = request.session
-        #получаем юзера по id
         advertisement = get_advertisement(advertisement_id)
-        # advertisement = self.session.get(Advertisement, advertisement_id)
         self.session.delete(advertisement)
         self.session.commit()
         return jsonify({'status': 'ok'})
